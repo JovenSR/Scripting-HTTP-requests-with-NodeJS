017c438d802eb686151eeda55eb85436508c9bf2
JovenSR <jovensrai@gmail.com>
2018-06-11 19:35:24 +0000

    appended chunks to a variable

---
+++ getAndPrintHTML.js
@@ -0,0 +1,22 @@
+var https = require('https');
+
+function getAndPrintHTML () {
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step2.html'
+  };
+  https.get(requestOptions, function(response) {
+    response.setEncoding('utf8');
+    response.on('data', function(data) {
+      const chunks = [];
+      chunks.push(data);
+      console.log(chunks);
+    });
+    response.on('end', function() {
+      console.log('Response stream complete.');
+    });
+  });
+
+}
+
+getAndPrintHTML();
